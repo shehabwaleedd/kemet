4ad0deb05a1c55b891760db2d0ccc95241e8dce5
Shehab Waleed <shehabwaleedd@gmail.com>
2023-08-13 06:16:36 +0400

    finished the left card of the lower container in home page.

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,8 @@ import './Home.css'
 import kadesh from "../../assets/kadesh-battle.jpeg"
 import { HiOutlineArrowNarrowRight } from 'react-icons/hi'
 import islamicCairo from '../../assets/yousef-salhamoud-VFhZGWWFF80-unsplash.jpg'
+import mohammadali from '../../assets/mohammad-ali.jpeg'
+import { TbDots } from "react-icons/tb"
 
 const Home = () => {
     return (
@@ -22,18 +24,33 @@ const Home = () => {
                 <div className="home__right">
                     <div className="hr__container">
                         <div className="hr__upper">
-                            <div className="hr__upper-left">
-                                <span>Culture</span>
-                                <h1>Egyptian influence on Persia in the Pre-Islamic period</h1>
-                                <p>In the fields of artistic work, architecture and sculpture, the Persians do not seem to have had any lasting impact on Egyptian tradition, during either both Achaemenid occupations of Egypt</p>
+                            <div className="hr__upper-container">
+                                <div className="hr__upper-left">
+                                    <span>Culture</span>
+                                    <h1>Egyptian influence on Persia in the Pre-Islamic period</h1>
+                                    <p>In the fields of artistic work, architecture and sculpture, the Persians do not seem to have had any lasting impact on Egyptian tradition, during either both Achaemenid occupations of Egypt</p>
+                                </div>
+                                <div className="hr__upper-right">
+                                    <img src={islamicCairo} alt="" />
+                                </div>
                             </div>
-                            <div className="hr__upper-right">
-                                <img src={islamicCairo} alt="" />
+                            <div className="get_started">
+                                <p>Get Started</p>
+                                <HiOutlineArrowNarrowRight className='arrow-right' />
                             </div>
                         </div>
                         <div className="hr__lower">
                             <div className="hr__lower-left">
-
+                                <img src={mohammadali} alt="" />
+                                <div className="hr__lower-title">
+                                    <h1>Portrait</h1>
+                                    <TbDots className='dots' />
+                                </div>
+                                <span> portrait of Muhammad Ali (1769–1849). He was a commander in the Ottoman army and had seized power after a civil war.</span>
+                                <div className="get_started-lower">
+                                    <p>Get Started</p>
+                                    <HiOutlineArrowNarrowRight className='arrow-right' />
+                                </div>
                             </div>
                             <div className="hr__lower-right">
 
